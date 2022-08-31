3ffb7e8a19c5fe048a286ecb9cbf599b45a503a4
flook542 <75408968+flook542@users.noreply.github.com>
2022-08-31 15:52:51 +0700

    update

---
+++ script.js
@@ -0,0 +1,27 @@
+function check() {
+    $.getJSON('/devphuwarin.github.io/data.json', function (data) {
+        let tk_number2 = document.getElementById('tracking_number').value;
+        let chk_number = 'false';
+        for (let i = 0; i < data.length; i++) {
+            if (data[i]['number'] == tk_number2) {
+                chk_number = 'true';
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Oops...',
+                    text: 'มีข้อมูลที่ตรงกัน!'
+                })
+                break;
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'ไม่มีข้อมูล!'
+                })
+            }
+        }
+        console.log(chk_number);
+        console.log(tk_number2);
+
+    });
+
+}
