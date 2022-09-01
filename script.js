05b9c71747c3e41613d704c0a915a071b4823c99
flook542 <75408968+flook542@users.noreply.github.com>
2022-09-01 16:37:16 +0700

    update

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-function check() {
-    fetch('data.csv')
-    .then(response => response.text())
-    .then(data => {
-        data = JSON.parse(data)
-        let tk_number2 = document.getElementById('tracking_number').value;
-        for (let i = 0; i < data.length; i++) {
-            if (data[i]['number'] == tk_number2) {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Oops...',
-                    text: 'มีข้อมูลที่ตรงกัน!'
-                })
-                break;
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'ไม่มีข้อมูล!'
-                })
-            }
-        }
-    });
+// function check() {
+//     fetch('data.csv')
+//     .then(response => response.text())
+//     .then(data => {
+//         data = JSON.parse(data)
+//         let tk_number2 = document.getElementById('tracking_number').value;
+//         for (let i = 0; i < data.length; i++) {
+//             if (data[i]['number'] == tk_number2) {
+//                 Swal.fire({
+//                     icon: 'success',
+//                     title: 'Oops...',
+//                     text: 'มีข้อมูลที่ตรงกัน!'
+//                 })
+//                 break;
+//             } else {
+//                 Swal.fire({
+//                     icon: 'error',
+//                     title: 'Oops...',
+//                     text: 'ไม่มีข้อมูล!'
+//                 })
+//             }
+//         }
+//     });
     
-    // $.getJSON('/devphuwarin.github.io/data.json', function (data) {
-    //     let tk_number2 = document.getElementById('tracking_number').value;
-    //     let chk_number = 'false';
-    //     for (let i = 0; i < data.length; i++) {
-    //         if (data[i]['number'] == tk_number2) {
-    //             chk_number = 'true';
-    //             Swal.fire({
-    //                 icon: 'success',
-    //                 title: 'Oops...',
-    //                 text: 'มีข้อมูลที่ตรงกัน!'
-    //             })
-    //             break;
-    //         } else {
-    //             Swal.fire({
-    //                 icon: 'error',
-    //                 title: 'Oops...',
-    //                 text: 'ไม่มีข้อมูล!'
-    //             })
-    //         }
-    //     }
-    //     console.log(chk_number);
-    //     console.log(tk_number2);
+//     // $.getJSON('/devphuwarin.github.io/data.json', function (data) {
+//     //     let tk_number2 = document.getElementById('tracking_number').value;
+//     //     let chk_number = 'false';
+//     //     for (let i = 0; i < data.length; i++) {
+//     //         if (data[i]['number'] == tk_number2) {
+//     //             chk_number = 'true';
+//     //             Swal.fire({
+//     //                 icon: 'success',
+//     //                 title: 'Oops...',
+//     //                 text: 'มีข้อมูลที่ตรงกัน!'
+//     //             })
+//     //             break;
+//     //         } else {
+//     //             Swal.fire({
+//     //                 icon: 'error',
+//     //                 title: 'Oops...',
+//     //                 text: 'ไม่มีข้อมูล!'
+//     //             })
+//     //         }
+//     //     }
+//     //     console.log(chk_number);
+//     //     console.log(tk_number2);
 
-    // });
+//     // });
 
-}
+// }
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,58 +1,58 @@
-function check() {
+// function check() {
     
-    const sheetId = '1pi6ID3iVvfr89kp33LcvN2UTPckat1zWuNyO6vA_I6E';
-    const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
-    const sheetName = 'data';
-    const query = encodeURIComponent('Select *')
-    const url = `${base}&sheet=${sheetName}&tq=${query}`
+//     const sheetId = '1pi6ID3iVvfr89kp33LcvN2UTPckat1zWuNyO6vA_I6E';
+//     const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
+//     const sheetName = 'data';
+//     const query = encodeURIComponent('Select *')
+//     const url = `${base}&sheet=${sheetName}&tq=${query}`
     
-    // const data = []
-    // document.addEventListener('DOMContentLoaded', init)
+//     // const data = []
+//     // document.addEventListener('DOMContentLoaded', init)
     
-    // const output = document.querySelector('.output')
+//     // const output = document.querySelector('.output')
     
-    //function init() {
-        fetch(url)
-            .then(res => res.text())
-            .then(data => {
-            data = JSON.parse(data.substring(47).slice(0, -2));
-            let tk_number2 = document.getElementById('tracking_number').value;
-            console.log(data['table']['rows']);
-            var result = [];
+//     //function init() {
+//         fetch(url)
+//             .then(res => res.text())
+//             .then(data => {
+//             data = JSON.parse(data.substring(47).slice(0, -2));
+//             let tk_number2 = document.getElementById('tracking_number').value;
+//             console.log(data['table']['rows']);
+//             var result = [];
 
-            for(var i in data['table']['rows'])
-                result.push([i, data['table']['rows'] [i]]);
+//             for(var i in data['table']['rows'])
+//                 result.push([i, data['table']['rows'] [i]]);
 
 
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows(result);
-            console.log(result);
-            for (let i = 0; i < data['table']['rows'].length; i++) {
-                console.log(data['table']['rows'].length);
-                console.log(data['table']['rows'][i]['c']);
-                console.log(data['table']['rows'][i]['c'][i]);
-                console.log(data['table']['rows'][i]['c'][i]['v']);
-                console.log(tk_number2);
-                if (data['table']['rows'][i]['c'][i]['v'] === tk_number2) {
-                    console.log('ตรงกัน');
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Oops...',
-                        text: 'มีข้อมูลที่ตรงกัน!'
-                    })
-                    break;
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'ไม่มีข้อมูล!'
-                    })
-                }
+//             var data = new google.visualization.DataTable();
+//             data.addColumn('string', 'Topping');
+//             data.addColumn('number', 'Slices');
+//             data.addRows(result);
+//             console.log(result);
+//             for (let i = 0; i < data['table']['rows'].length; i++) {
+//                 console.log(data['table']['rows'].length);
+//                 console.log(data['table']['rows'][i]['c']);
+//                 console.log(data['table']['rows'][i]['c'][i]);
+//                 console.log(data['table']['rows'][i]['c'][i]['v']);
+//                 console.log(tk_number2);
+//                 if (data['table']['rows'][i]['c'][i]['v'] === tk_number2) {
+//                     console.log('ตรงกัน');
+//                     Swal.fire({
+//                         icon: 'success',
+//                         title: 'Oops...',
+//                         text: 'มีข้อมูลที่ตรงกัน!'
+//                     })
+//                     break;
+//                 } else {
+//                     Swal.fire({
+//                         icon: 'error',
+//                         title: 'Oops...',
+//                         text: 'ไม่มีข้อมูล!'
+//                     })
+//                 }
                 
-            }
-        });
-    //}
-}
+//             }
+//         });
+//     //}
+// }
   
